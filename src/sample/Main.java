6d06d735ec05af568819c37ae5c5dd194f5826e3
BOUMAZA TARIK p1712760 tarik.boumaza@etu.univ-lyon1.fr <tarik.boumaza@etu.univ-lyon1.fr>
2021-05-27 10:38:15 +0200

    initialisation grille

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -19,6 +19,9 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        //launch(args);
+
+        Grille grille = new Grille(4, 5);
+        System.out.println(grille);
     }
 }
